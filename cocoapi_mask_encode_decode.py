815bf0340072719659cf8747a85e1643d9b879c5
Sharif Elfouly <s.elfouly@enbw.com>
2020-01-16 10:07:52 +0100

    decode string byte rle encoded mask

--- cocoapi_mask_encode_decode.py
+++ cocoapi_mask_encode_decode.py
@@ -5,3 +5,10 @@ n_a = np.random.normal(size=(10, 10))
 b_a = np.array(n_a > 0.5, dtype=np.bool, order='F')
 e_a = m.encode(b_a)
 d_a = m.decode(e_a)
+
+# Decode byte string rle encoded mask.
+sz = pred['mask']['size']
+c = pred['mask']['counts'][2:-1]
+es = str.encode(c)
+t = {'size': [450, 800], 'counts': es}
+dm = m.decode(t)
